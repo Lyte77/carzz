f37937c0240cbaebfb76d5f7b68bc2e0564e7caf
Lyte <litecode77@gmail.com>
2024-11-20 02:52:37 -0800

    worked on registering users

---
+++ account/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+app_name = 'account'
+
+urlpatterns = [
+    path('sign_up/', views.register_user, name='register')
+]
---
+++ account/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
+from django.contrib.auth import login,logout
+
+# Create your views here.
+
+def register_user(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request,user)
+            
+
+    else:
+        form = CustomUserCreationForm()
+    return render(request,'account/register.html',{'form':form})
+
